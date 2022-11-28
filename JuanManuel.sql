f088a52314176952d52a9de8208c65eb184a87f0
Juan_Manuel <juanma26@gmail.com>
2022-11-28 15:54:56 -0300

    mejora 5

---
+++ JuanManuel.sql
@@ -0,0 +1,17 @@
+
+
+create database JuanManuel;
+
+
+use JuanManuel;
+
+
+
+
+create table contactos(
+idContacto int unsigned not null auto_increment,
+nombre varchar(150) not null,
+email varchar(150) not null,
+primary key (idContacto)
+);
+
